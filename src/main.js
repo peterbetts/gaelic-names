4499b86e0d8e18ada2f4c57920d5585e6487ff2b
Sean Morrow <s3anmorrow@gmail.com>
2024-03-04 08:31:07 -0400

    initial

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// importing the sass stylesheet for bundling
+import "./../sass/styles.scss";
+// importing from Toolkit module
+import { getJSONData } from "./Toolkit";
+
+// defined the web app's data source
+const SOURCE = "https://data.novascotia.ca/resource/wwaq-2akq.json";
+
+// --------------------------------------------------------- main method
+function main() {
+
+}
+
+main();
